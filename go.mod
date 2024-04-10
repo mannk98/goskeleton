d58cee7df8befdb285e5f7adc0660a9356d09201
mannk <khacman98@gmail.com>
2024-04-11 01:07:09 +0700

    update

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/huntelaar112/gotools
+
+go 1.15
+
+require (
+	github.com/spf13/cobra v1.6.1
+	github.com/spf13/viper v1.12.0
+)
---
+++ tpl/main.go
@@ -0,0 +1,193 @@
+// Copyright © 2021 Steve Francia <spf@spf13.com>.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tpl
+
+func MainTemplate() []byte {
+	return []byte(`/*
+{{ .Copyright }}
+{{ if .Legal.Header }}{{ .Legal.Header }}{{ end }}
+*/
+package main
+
+import "{{ .PkgName }}/cmd"
+
+func main() {
+	cmd.Execute()
+}
+`)
+}
+
+func RootTemplate() []byte {
+	return []byte(`/*
+{{ .Copyright }}
+{{ if .Legal.Header }}{{ .Legal.Header }}{{ end }}
+*/
+package cmd
+
+import (
+{{- if .Viper }}
+	"fmt"{{ end }}
+	"os"
+	log "github.com/sirupsen/logrus"
+	"github.com/huntelaar112/netutils"
+	"github.com/huntelaar112/goutils/utils"
+
+	"github.com/spf13/cobra"
+{{- if .Viper }}
+	"github.com/spf13/viper"{{ end }}
+)
+
+var (
+{{ if .Viper -}}
+	cfgFile string
+{{- end }}
+	Logger   = log.New()
+	LogLevel = log.ErrorLevel
+	LogFile  = "{{ .AppName }}.log"
+	cfgFileDefault = ".{{ .AppName }}"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "{{ .AppName }}",
+	Short: "A brief description of your application",
+	Long: ` + "`" + `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.` + "`" + `,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: rootRun,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	utils.InitLogger(LogFile, Logger, LogLevel)
+{{- if .Viper }}
+	cobra.OnInitialize(initConfig)
+{{ end }}
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+{{ if .Viper }}
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.{{ .AppName }}.yaml)")
+{{ else }}
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.{{ .AppName }}.yaml)")
+{{ end }}
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+{{ if .Viper -}}
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		if err != nil {
+			Logger.Error(err)
+			os.Exit(1)
+		}
+
+		cfgFile=cfgFileDefault
+		// Search config in home directory with name ".{{ .AppName }}" (without extension).
+		viper.AddConfigPath(home)
+		viper.AddConfigPath("./")
+		viper.SetConfigType("toml")
+		viper.SetConfigName(cfgFile)
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Error("config.toml file at ./ folder is not exist. Create it first.")
+		} else {
+			Logger.Error(err)
+		}
+	} else {
+		Logger.Info("Using config file:", viper.ConfigFileUsed())
+	}
+}
+{{- end }}
+
+func rootRun(cmd *cobra.Command, args []string) {
+
+}
+`)
+}
+
+func AddCommandTemplate() []byte {
+	return []byte(`/*
+{{ .Project.Copyright }}
+{{ if .Legal.Header }}{{ .Legal.Header }}{{ end }}
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/huntelaar112/netutils"
+	"github.com/huntelaar112/goutils/utils"
+
+	"github.com/spf13/cobra"
+)
+
+// {{ .CmdName }}Cmd represents the {{ .CmdName }} command
+var {{ .CmdName }}Cmd = &cobra.Command{
+	Use:   "{{ .CmdName }}",
+	Short: "A brief description of your command",
+	Long: ` + "`" + `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.` + "`" + `,
+	Run: {{ .CmdName }}Run,
+}
+
+func init() {
+	{{ .CmdParent }}.AddCommand({{ .CmdName }}Cmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// {{ .CmdName }}Cmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// {{ .CmdName }}Cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func {{ .CmdName }}Run(cmd *cobra.Command, args []string) {
+	Logger.Info("cmd called")
+}
+
+`)
+}
